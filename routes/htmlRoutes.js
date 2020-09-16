3fa5379f63f14d8a35ab8d0eda58b86ac353e8fd
Ibrahim Riyami <iariyami@ncsu.edu>
2020-09-15 22:53:39 -0400

    added html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = function (app) {
+    // Sets index.html page as the initial landing page for server
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/index.html"));
+    });
+
+    // Sets exercise.html page as the /exercise route on server
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/exercise.html"));
+    });
+
+    // Sets exercise.html page as the /exercise route on server
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/stats.html"));
+    });
+};
