0a5f2d1cc38c66eee3bfc58193f20f17e91ee373
Ibrahim Riyami <iariyami@ncsu.edu>
2020-10-01 11:30:38 -0400

    added workout arrays

--- models/workout.js
+++ models/workout.js
@@ -4,6 +4,39 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
     {
+        day: {
+            type: Date,
+            required: [true, "day is required"],
+        },
+
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter exercises.",
+                },
+                name: {
+                    type: String,
+                    required: " Name of the exercise.",
+                },
+                duration: {
+                    type: Number,
+                },
+                weight: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                },
+            },
+        ],
     });
 
 module.exports = workout;
